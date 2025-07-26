1873ba3c98db8902dea6b96a50f06f05d89d6a1c
AEiOuf <franzpatrick123@gmail.com>
2025-07-26 20:02:16 +0800

    Changes by Edriene Paingan

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,49 @@ public class Main {
     public static final Scanner scanner = new Scanner(System.in);
     public static ArrayList<ArrayList<Integer>> relationshipGraph = new ArrayList<>();
     public static void main(String[] args) {
+        /*
+        PART 1: Loading the Graph
+        - must accept as input the file name or file path of the text file
+        - must be stored in appropriate data structure after reading
+        - must display a menu for two features
+            > display friend list
+            > display connections
+         */
+        FileReader fr = new FileReader();
+        RelationshipRecord[] relationships = fr.readFile("Network1.txt");
 
+        for (int j = 0; j < 5000; j++){
+            relationshipGraph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < relationships.length; i++){
+            // relationships[i] = (1, 2)
+            int person1 = relationships[i].getPerson1();
+            int person2 = relationships[i].getPerson2();
+
+            relationshipGraph.get(person1).add(person2);
+            relationshipGraph.get(person2).add(person1);
+        }
+
+
+        for (int k = 0; k <relationships.length; k++){
+            int[] friends = new int[2500];
+            int number = 0;
+
+
+            for (int l = 0; l < relationshipGraph.get(k).size(); l++){
+                friends[l] = relationshipGraph.get(k).get(l);
+                number += 1;
+            }
+
+            System.out.println("friend of "+ k + "is ");
+
+            for (int m = 0; m < number ; m++){
+                System.out.println(friends[m]);
+            }
+        }
+
+        displayMenu();
     }
 
     public static void displayMenu() {
